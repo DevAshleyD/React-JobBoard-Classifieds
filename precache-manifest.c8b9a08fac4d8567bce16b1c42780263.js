c038563bb7636fb14a036dcf07251f32abaa1d9d
Ashley D <webdevmvp@gmail.com>
2019-12-02 00:54:25 +0000

    Updates

---
+++ precache-manifest.c8b9a08fac4d8567bce16b1c42780263.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6996a1bcbbc4b4062c666db6f41fdd1c",
+    "url": "/React-JobBoard/index.html"
+  },
+  {
+    "revision": "b669d312b5563a615d48",
+    "url": "/React-JobBoard/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d8272c0810214caaa8c5",
+    "url": "/React-JobBoard/static/css/main.944b14ca.chunk.css"
+  },
+  {
+    "revision": "b669d312b5563a615d48",
+    "url": "/React-JobBoard/static/js/2.b73e0eb8.chunk.js"
+  },
+  {
+    "revision": "d8272c0810214caaa8c5",
+    "url": "/React-JobBoard/static/js/main.98d770a6.chunk.js"
+  },
+  {
+    "revision": "4d197bf55bd0bb9cc232",
+    "url": "/React-JobBoard/static/js/runtime-main.4b2171fc.js"
+  }
+]);
